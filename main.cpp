48d2b249f79c2859849850d9bd6ed6d2113ab352
xBloodLotusx <56528145+xBloodLotusx@users.noreply.github.com>
2025-04-22 22:32:31 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,12 +7,7 @@
 
 int main() {
     std::cout << "This program is intended to predict a team's chance of winning!\nInput a digit specifying which command and variables accordingly.\nUse _ for the spaces in a team's name i.e. 2 Real_Madrid Manchester_United.\n1: Team\n2: Team Opponent\n3: Quit" << std::endl;
-    std::vector<Match> data = {
-            Match(20230101, "Real_Madrid", "Barcelona", 2, 1),
-            Match(20230201, "Real_Madrid", "Atletico_Madrid", 3, 0),
-            Match(20230301, "Barcelona", "Real_Madrid", 1, 1),
-            Match(20230401, "Real_Madrid", "Sevilla", 4, 1)
-    };
+
     std::string line;
     std::getline(std::cin, line);
     std::istringstream stream(line);
